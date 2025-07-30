97bcb7e09897521d07d36b15f9c9a5398ffc67f7
Samragyee Dhakal <samragyeedhakal@gmail.com>
2025-07-29 20:48:40 -0400

    Testing out selenium

--- reddit.py
+++ reddit.py
@@ -1,2 +1,14 @@
 from selenium import webdriver
-#adding comment to test the branch 
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+
+driver.get('https://selenium.dev/documentation')
+assert 'Selenium' in driver.title
+
+elem = driver.find_element(By.ID, 'm-documentationwebdriver')
+elem.click()
+assert 'WebDriver' in driver.title
+
+driver.quit()
