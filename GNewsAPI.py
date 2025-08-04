33fb6e006d889b0fc5af8e66ef621f5b407a73b1
Mita <rwiriya123@gmail.com>
2025-08-04 15:03:12 -0400

    getting content from article URLs

--- GNewsAPI.py
+++ GNewsAPI.py
@@ -7,21 +7,38 @@ import urllib.request
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
-# Query parameters are adjusted in this url
-url = f"https://gnews.io/api/v4/search?q=Google&lang=en&max=10&from=2015-07-18T21:32:58.500Z&to=2020-07-18T21:32:58.500Z&apikey={API_KEY}"
+from newspaper import Article
 
-# Header row
-df = pd.DataFrame({'title': [], 'url': []})
+# Call API and write results to CSV
+def getArticles():
+       load_dotenv()
+       API_KEY = os.getenv("API_KEY")
+       # Query parameters are adjusted in this url
+       url = f"https://gnews.io/api/v4/search?q=Google&lang=en&max=10&from=2023-07-18T21:32:58.500Z&to=2024-07-18T21:32:58.500Z&apikey={API_KEY}"
+       # Header row
+       df = pd.DataFrame({'title': [], 'url': []})
 
-with urllib.request.urlopen(url) as response:
-    data = json.loads(response.read().decode("utf-8"))
-    articles = data["articles"]
-    for i in range(len(articles)):
-        # Get new row data
-        newRow = {'title': articles[i]["title"],
-                'url': articles[i]["url"]}
-        df.loc[len(df)] = newRow
+       with urllib.request.urlopen(url) as response:
+              data = json.loads(response.read().decode("utf-8"))
+              articles = data["articles"]
+              for i in range(len(articles)):
+                      # Get new row data
+                     newRow = {'title': articles[i]["title"],
+                      'url': articles[i]["url"]}
+                     df.loc[len(df)] = newRow
+        # Write titles and URLS to csv
+       df.to_csv('articles.csv', index=False)
 
-df.to_csv('articles.csv', index=False)
+# Get content from articles.csv
+def getArticleContent():
+       # Get dataframe from articles.csv
+       df = pd.read_csv("articles.csv")
+       for url in df['url']:
+              # Print URL and article content
+              print("URL:", url)
+              article = Article(url)
+              article.download()
+              article.parse()
+              print("Content:", article.text)
+#getArticles()
+getArticleContent()
