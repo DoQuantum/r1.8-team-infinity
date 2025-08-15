c66006c9b1b32a6dfdeaca830e410962f976e010
Samragyee Dhakal <samragyeedhakal@gmail.com>
2025-08-15 14:34:39 -0400

    Using PRAW instead of selenium

--- reddit.py
+++ reddit.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 driver = webdriver.Chrome()
 
-driver.get('https://www.reddit.com/r/wallstreetbets/?feedViewType=compactView')
-assert 'Reddit' in driver.title
+driver.get('https://old.reddit.com')
+assert 'reddit' in driver.title
 
-elem = driver.find_element(By.ID, 'absolute inset-0')
+elem = driver.find_element(By.ID, 'thing_t3_1mco73c')
 elem.click()
 assert 'WebDriver' in driver.title
 
---
+++ scrape.py
@@ -0,0 +1,19 @@
+import praw
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+Client_ID = os.getenv('CLIENT_ID')
+Client_Secret = os.getenv('CLIENT_SECRET')
+User_Agent = os.getenv('USER_AGENT')
+reddit = praw.Reddit(
+    client_id=Client_ID,
+    client_secret=Client_Secret,
+    user_agent=User_Agent,
+)
+
+for submission in reddit.subreddit("wallstreetbets").hot(limit=10):
+    print(submission.body)
