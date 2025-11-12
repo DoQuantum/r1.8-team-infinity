db45c083b37a048d2fb81599dd32a1da13aae765
rwiriya <rwiriya123@gmail.com>
2025-11-11 19:43:31 -0500

    Update combine.py

--- combine.py
+++ combine.py
@@ -9,16 +9,12 @@ import re
 from datetime import datetime
 
 target_stock = "Amazon"
-
 csv_files = [f'readingArticles_{target_stock}.csv', f'{target_stock}_sentiments.csv']
 
 # Load each CSV file into a DataFrame and store in a list
 dataframes = [pd.read_csv(csv_file) for csv_file in csv_files]
-
 dataframes[0].drop('authors') # Temp - remove authors column in news csv
 dataframes[1].rename(columns={'created_date': 'date', 'body': 'content'})
-
 # Concatenate all DataFrames into a single DataFrame
 csvFile = pd.concat(dataframes, ignore_index=True)
-
-csvFile.to_csv(f"{target_stock}_all.csv")
+csvFile.to_csv(f"{target_stock}_combined.csv")
